a156bc543b2b2ced12f3401e5951a2f1dc9ee385
Stephanie <stephaniemondejar.22@gmail.com>
2023-04-24 12:32:20 +0800

    Merge branch 'AngelBranch' of https://github.com/mangeltk/CaSpaceApplication into StephanieBranch
     Conflicts:
    	app/build.gradle
    	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/caspaceapplication/Owner/Owner_BottomNavigationBar.java
@@ -0,0 +1,45 @@
+package com.example.caspaceapplication.Owner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.caspaceapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Owner_BottomNavigationBar extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_bottom_navigation_bar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.owner_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuHome:
+                startActivity(new Intent(this, OwnerHomepage.class));
+                return true;
+            case R.id.menuMessages:
+                //startActivity(new Intent(this, MessageActivity.class));
+                return true;
+            case R.id.menuNotification:
+                //startActivity(new Intent(this, NotificationActivity.class));
+                return true;
+            case R.id.menuProfile:
+                //startActivity(new Intent(this, OwnerProfileActivity.class));
+                return true;
+            default:
+                return false;
+        }
+    }
+}
